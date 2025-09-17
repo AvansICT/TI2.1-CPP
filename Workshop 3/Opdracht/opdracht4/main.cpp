8c0422b8dc8cb04e8030cc4e4abd2b7d66ad4886
Marcel JP Vissers <mjp.vissers@avans.nl>
2025-09-17 21:08:34 +0200

    Add opdracht 4

---
+++ Workshop 3/Opdracht/opdracht4/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    const int studentId;       // kan niet aangepast worden na constructie
+    static int aantalStudenten; // gedeeld door alle Student-objecten
+
+public:
+    Student(int id) : studentId(id) {
+        // TODO: verhoog aantalStudenten
+    }
+
+    int getStudentId() const {
+        // TODO: retourneer de ID
+		return 0; // placeholder
+    }
+
+    static int getAantalStudenten() {
+        // TODO: retourneer aantalStudenten
+		return 0; // placeholder
+    }
+};
+
+// TODO: initialiseer static variabele
+
+int main() {
+    Student s1(101);
+    Student s2(102);
+
+    cout << "Student 1 ID: " << s1.getStudentId() << endl;
+    cout << "Student 2 ID: " << s2.getStudentId() << endl;
+
+    cout << "Totaal aantal studenten: " << Student::getAantalStudenten() << endl;
+}
