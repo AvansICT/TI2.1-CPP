2087ebc014aede84d8ec57ded7b14863a5ae0250
Marcel JP Vissers <mjp.vissers@avans.nl>
2025-09-23 09:58:31 +0200

    Add workshop 4 skeleton

---
+++ Workshop 4/Workshop/workshop/main.cpp
@@ -0,0 +1,12 @@
+// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "log.hpp"
+
+int main()
+{
+    std::cout << "Workshop 4!\n";
+    std::cout << __DATE__ << " " << __TIME__ << std::endl; // log date and time of compilation, not runtime
+    return 0;
+}
