46bf4778f9c63ab9fe015feb77dfdeca9d753930
Marcel JP Vissers <mjp.vissers@avans.nl>
2025-10-02 14:05:45 +0200

    Setup workshop 6

---
+++ Workshop 6/Workshop/workshop/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "Workshop 6!\n";
+    std::cout << __DATE__ << " " << __TIME__ << std::endl; // log date and time of compilation, not runtime
+    return 0;
+}
