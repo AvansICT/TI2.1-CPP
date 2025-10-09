485f58186bb50bbd5435f0bc8691d567c193a3cf
Marcel JP Vissers <mjp.vissers@avans.nl>
2025-10-09 13:59:01 +0200

    Fix main

--- Workshop 6/Opdracht/reductie/main.cpp
+++ Workshop 6/Opdracht/reductie/main.cpp
@@ -34,20 +34,6 @@ int main() {
 
 	// TODO Parallelle berekening van som met OpenMP reductie
 
-    // ------------------------
-    // Parallel met OpenMP reductie
-    // ------------------------
-    long long sum_parallel = 0;
-    t2.startTimer();
-    int iCPU = omp_get_num_procs(); // Get the number of processors in this system
-    omp_set_num_threads(iCPU); // Now set the number of threads
-#pragma omp parallel for reduction(+:sum_parallel)
-    for (long long i = 0; i < N; ++i) {
-        sum_parallel += data[i];
-    }
-    t2.stopTimer();
-    std::cout << "Parallel sum = " << sum_parallel
-        << ", Time = " << t2.elapsedTime() << " s\n";
 
     return 0;
 }
