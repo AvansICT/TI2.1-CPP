21e7f5f56a8d0fc262e1f69b56c592e1475e23b1
Marcel JP Vissers <mjp.vissers@avans.nl>
2025-10-21 23:22:21 +0200

    Add opdracht workshop 8

---
+++ Workshop 8/Opdracht/Opdracht/main.cpp
@@ -0,0 +1,11 @@
+// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+#include <print>    //C++23
+
+int main()
+{
+    std::print("Hello, Opdracht\n");    // C++23 feature
+
+    return 0;
+}
